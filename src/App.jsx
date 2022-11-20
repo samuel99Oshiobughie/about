db56298563f15e3e46f5378a22e753d0aed89653
Samuel oshiobughie <Cedar@MACs-MacBook-Pro.local>
2022-11-20 15:34:03 +0100

    feat: About page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 // import Notifications from './pages/Notifications';
 import Profile from './pages/Profile';
+// import About from './pages/About';
 import './App.css';
 
 function App() {
